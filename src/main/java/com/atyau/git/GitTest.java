bac90874fcc4f4ed7b99e756c7533e4adea372a3
superzhang <superzhang@qq.com>
2022-05-27 15:39:40 +0800

    hot-fix commit

--- src/main/java/com/atyau/git/GitTest.java
+++ src/main/java/com/atyau/git/GitTest.java
@@ -7,5 +7,8 @@ package com.atyau.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Git4!");
     }
 }
