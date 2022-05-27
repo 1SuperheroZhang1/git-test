cde8bcbdad342277fe67d25f060fa41d68db793d
superzhang <superzhang@qq.com>
2022-05-27 15:16:50 +0800

    first commit

---
+++ src/main/java/com/atyau/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atyau.git;
+
+/**
+ * @author superherozhang
+ *          2022-05-27 15:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
