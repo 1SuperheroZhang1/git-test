61266cc4dd5d3678b8bf1423af6c152ec4597f42
superzhang <superzhang@qq.com>
2022-05-27 15:22:16 +0800

    second commit

--- src/main/java/com/atyau/git/GitTest.java
+++ src/main/java/com/atyau/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atyau.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
