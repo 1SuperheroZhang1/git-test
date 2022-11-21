09c38b0e44e62c45c0b3476980ef3b701a8575f5
zhangcy <2304417358@qq.com>
2022-11-21 17:09:57 +0800

    array review2

--- src/main/java/com/atguigu/java/ArrayReview.java
+++ src/main/java/com/atguigu/java/ArrayReview.java
@@ -2,6 +2,7 @@ package com.atguigu.java;
 
 public class ArrayReview {
     public static void main(String[] args) {
+    //一、一维数组
         //1.一维数组的声明和初始化
         //声明
         int arr;
@@ -91,7 +92,50 @@ public class ArrayReview {
         for (int i = 0; i <booleanValue.length ; i++) {
             System.out.print(booleanValue[i]+" ");
         }
+        /**
+         * 二、二维数组
+         *   Java语言提供了支持多维数组的用法。
+         *   规定：二维数组分为外层数组元素、内层数组元素。
+         *   理解：对二维数组的理解，可以看成是一维数组arr1有作为另一个一维数组arr2的元素而存在。
+         *   从数组的底层机制来看，其实没有多维数组。
+         */
+        //1.二维数组的声明和初始化
+        //静态初始化
+        int array[][]=new int[][]{{1,2,3},{4,5},{6,7,8}};//声明+初始化
+        //动态初始化
+        String array1[][]=new String[4][3];
+        String array2[][]=new String[4][];
+        //err:
+//        String array3[][]=new String[][3];
+//        int array4[][]=new int[3][3]{{1,2,3},{4,5},{6,7,8}};//声明+初始化
+        int[] array5[]=new int[][]{{1,2,3,4},{5,6,7},{8,9,0}};//类型推断
 
+        //2.调用数组指定位置的元素 - 数组名[行标][列标]
+        System.out.println(array[1][0]);//4
+        System.out.println(array1[3][2]);//null
+
+        //3.获取数组的长度 - 数组名.length
+        System.out.println(array.length);//3
+        System.out.println(array5[0].length);//4 数组名[行标].length 每行拥有的元素个数
+
+        //4.遍历二维数组 - 双层for循环
+        for (int i = 0; i <array5.length ; i++) {
+            for (int j = 0; j <array5[i].length ; j++) {
+                System.out.print(array5[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        //5.数组元素的默认初始化值
+        int[] ints[]=new int[4][3];
+        //外层元素的初始化值：数组名[行标]默认值：数组名[行标][0]元素的首地址
+        System.out.println(ints[1]);//[I@26ba2a48
+        //内层元素的初始化值：数组名[行标][列标]默认值：与一维数组一致
+        System.out.println(ints[1][0]);//0
+
+        float[] floats[]=new float[4][];
+        System.out.println(floats[1]);//null
+        System.out.println(floats[2][3]);//err - NullPointerException
     }
 
 }
