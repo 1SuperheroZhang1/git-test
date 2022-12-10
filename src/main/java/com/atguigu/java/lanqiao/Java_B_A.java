70c342c04999c450b294e749640a1a288f8e1bfc
zhangcy <2304417358@qq.com>
2022-12-10 21:33:46 +0800

    第十三届蓝桥杯大赛Java B组第一题_第二题

--- src/main/java/com/atguigu/java/lanqiao/Java_B_A.java
+++ src/main/java/com/atguigu/java/lanqiao/Java_B_A.java
@@ -7,9 +7,13 @@ import org.junit.Test;
  * */
 public class Java_B_A {
     //A.已知今天是星期六，请问20^22天后是星期几？ 注意：用数字1到7表示星期一到星期日
+    /*
+    * 解题思路：暴力求解法
+    *  直接利用计算机计算20^22，再对此数进行取余(7)，结果为1.
+    * */
     public static void main(String[] args) {
         int index=6;
-        int result= (int) ((index+Math.pow(20,22))%7);
-        System.out.println(result);//周一
+        int result= (int) (index+Math.pow(20,22)%7);
+        System.out.println(result);//7 - 周日
     }
 }
