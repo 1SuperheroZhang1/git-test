1d1826054108e7eaddec7ddf93bfd61b74dd2882
zhangcy <2304417358@qq.com>
2022-12-29 22:08:26 +0800

    第十三届蓝桥杯大赛Java B组练习系统_ALGO_3_用到的算法_动态规划

---
+++ src/main/java/com/atguigu/java/lanqiao/exer/ALGO_3.java
@@ -0,0 +1,26 @@
+package com.atguigu.java.lanqiao.exer;
+
+import java.util.Scanner;
+
+/*
+* 【问题描述】
+    如果一个自然数N的K进制表示中任意的相邻的两位都不是相邻的数字，那么我们就说这个数是K好数。
+    求L位K进制数中K好数的数目。例如K = 4，L = 2的时候，所有K好数为11、13、20、22、30、31、33 共7个。
+    由于这个数目很大，请你输出它对1000000007取模后的值。
+ 【输入格式】
+    输入包含两个正整数，K和L。
+ 【输出格式】
+    输出一个整数，表示答案对1000000007取模后的值。
+ 【样例输入】
+    4 2
+ 【样例输出】
+    7
+  用到的算法：动态规划！！！！
+* */
+public class ALGO_3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int l = sc.nextInt();
+
+    }
+}
